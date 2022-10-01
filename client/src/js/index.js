c2a40d8480d664010a17c3cd19c17eb44aac11ed
rva-mike <mike.levy.rva@gmail.com>
2022-10-01 15:39:40 -0400

    injectManifest class

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -98,6 +98,12 @@ window.editCard = (e) => {
 };
 
 
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+  navigator.serviceWorker.register('./service-worker.js');
+})};
+
 
 // Import CSS files
 import "../css/index.css";
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,25 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      title: 'Webpack Plugin',
-    })
-  ],
+
+plugins: [
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    title: 'Webpack Plugin',
+  }),
+  new InjectManifest({
+    swSrc: './src/sw.js',
+    swDest: 'service-worker.js',
+  }),
+],
   module: {
     rules: [
       {

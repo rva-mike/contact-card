2c6e5ef9ff6c9d879fbdc6e7d1dcca06e2c3d7fc
rva-mike <mike.levy.rva@gmail.com>
2022-10-01 13:22:38 -0400

    delete functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,8 +2,8 @@ import "./form";
 // import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initDb, getDb, postDb } from './database';
-import {fetchCards} from './cards';
+import { initDb, getDb, postDb, deleteDb } from './database';
+import { fetchCards } from './cards';
 import { toggleForm, clearForm } from './form';
 
 
@@ -21,43 +21,51 @@ window.addEventListener('load', function () {
 });
 
 
-  // Form functionality
-  const form = document.getElementById("formToggle");
-  const newContactButton = document.getElementById("new-contact");
-  let submitBtnToUpdate = false;
-  let profileId;
-  
-  newContactButton.addEventListener('click', event => {
-    toggleForm()
-   })
-  
-  form.addEventListener('submit', event => {
-    // Handle data
-    event.preventDefault();
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+})
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
   let name = document.getElementById("name").value;
   let phone = document.getElementById("phone").value;
   let email = document.getElementById("email").value;
   let profile = document.querySelector('input[type="radio"]:checked').value;
-  
-    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
-  
+
     fetchCards();
-      // Toggles the submit button back to POST functionality
+    // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
   }
-  
+
   // Clear form
   clearForm();
   // Toggle form
   toggleForm();
   // Reload the DOM
   fetchCards();
-  });
+});
 
 
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
 
 // Import CSS files
 import "../css/index.css";
